a7a601245a26f400e466a1225dde39184a499ce2
Akhan Ismailov <404akhan@gmail.com>
2016-11-03 15:53:02 +0800

    add

--- logic/index.js
+++ logic/index.js
@@ -56,11 +56,134 @@ module.exports.postbackData = function(recipientId, postback) {
     }
   };
 
+  /**************            **************/
+  var beautyServiceBook = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: "We offer Makeup and Facial Service. What do you want to book?",
+      quick_replies: [
+        {
+          "content_type":"text",
+          "title":"Makeup Service",
+          "payload":"Beauty Service Book Makeup Service"
+        },
+        {
+          "content_type":"text",
+          "title":"Makeup Class",
+          "payload":"Beauty Service Book Makeup Class"
+        },
+        {
+          "content_type":"text",
+          "title":"Facial Service",
+          "payload":"Beauty Service Book Facial Service"
+        }
+      ]
+    }
+  };
+
+  /**************            **************/
+  var service = "";
+
+  function fBeautyServiceBookGen(service) {
+
+    var firstMessage = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        text: "We offer different makeup services. Which do you want to join?",
+        metadata: "DEVELOPER_DEFINED_METADATA"
+      }
+    };
+
+    var element1 = {
+      title: "黃金比例個人彩粧服務 ($500 / 45分鐘)",
+      buttons: [{
+        type: "postback",
+        title: "Book",
+        payload: "Beauty Service Book Makeup " + service + " Book",
+      }, {
+        type: "postback",
+        title: "Details",
+        payload: "Beauty Service Book Makeup " + service + " Details"
+      }],
+    };
+
+    var element2 = {
+      title: "黃金比例重點化粧指導 ($900 / 60分鐘)",
+      buttons: [{
+        type: "postback",
+        title: "Book",
+        payload: "Beauty Service Book Makeup " + service + " Book",
+      }, {
+        type: "postback",
+        title: "Details",
+        payload: "Beauty Service Book Makeup " + service + " Details"
+      }],
+    };
+
+    var element3 = {
+      title: "黃金比例完整彩粧指導 ($1200 / 90分鐘)",
+      buttons: [{
+        type: "postback",
+        title: "Book",
+        payload: "Beauty Service Book Makeup " + service + " Book",
+      }, {
+        type: "postback",
+        title: "Details",
+        payload: "Beauty Service Book Makeup " + service + " Details"
+      }],
+    };
+
+    var returnData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            elements: [element1, element2, element3]
+          }
+        }
+      }
+    };
+
+    return [firstMessage, returnData];
+  }
+
+  /**************            **************/
+
+  /**************            **************/
+
 
   switch(postback) {
     case 'Get Started':
       dataArray.push(getStartedMessage);
       dataArray.push(getStartedTemplate);
+
+      break;
+
+    case 'Beauty Service Book':
+      dataArray.push(beautyServiceBook);
+
+      break;
+
+    case 'Beauty Service Book Makeup Service':
+      dataArray = fBeautyServiceBookGen('Makeup Service');
+
+      break;
+
+    case 'Beauty Service Book Makeup Class':
+      dataArray = fBeautyServiceBookGen('Makeup Class');
+
+      break;
+
+    case 'Beauty Service Book Facial Service':
+      dataArray = fBeautyServiceBookGen('Facial Service');
 
       break;
 
